711ecb83d853aa637a8f5b45d322413d00ceec47
Robyn Sanders <reddie@smartchat.net.au>
2021-10-19 21:45:53 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+/*const element = document.createElement('div');
+const element = document.createElement('ul');
+document.body.appendChild(element);
+const ul = document.createElement('ul');*/
+
+/*for (let i = 0; i < 3; i++) {
+    const li = document.createElement('li');
+    li.innerHTML = (i + 1).toString();
+    ul.appendChild(li);*/
+
+//}
+/*element.appendChild(ul);*/
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
+
+
